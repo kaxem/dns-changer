d43d793ff4aa18a7267ad6b664ffabf2f2b15cb4
kaaxem <mmdkazemm@gmail.com>
2023-01-08 15:24:39 +0330

    relocating resolv.conf

--- dns.sh
+++ dns.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Simple program to change nameservers
-sed -i '' "/nameserver/d" resolv.conf
+sed -i '' "/nameserver/d" /etc/resolv.conf
 echo '1.Manual Nameservers\n 2.Shekan nameservers {178.22.122.100 & 185.51.200.2}\n 3.Default {8.8.8.8}'
 read VAL
 if [ "$VAL" -eq "1" ]
@@ -9,13 +9,15 @@ if [ "$VAL" -eq "1" ]
 		while read INPUT 
 		do
 			[[ $INPUT ]] || break
-			sed -i -e '$s/$/\nnameserver '${INPUT}' \n/' resolv.conf
+			sed -i -e '$s/$/\nnameserver '${INPUT}' \n/' /etc/resolv.conf
 		done
  elif [ "$VAL" -eq "2" ]
 	then 
                sed -i -e '$s/$/nameserver 178.22.122.100\nnameserver 185.51.200.2\n/' resolv.conf
  elif [ "$VAL" -eq "3" ]
 	then
-		sed -i -e '$s/$/nameserver 8.8.8.8\n/' resolv.conf
+		sed -i -e '$s/$/nameserver 8.8.8.8\n/' /etc/resolv.conf
 else exit
 fi
+echo 'done!'
+exit
